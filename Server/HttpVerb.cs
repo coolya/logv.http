ef63ff32f43e7e5f6d49cefe83b137e0ffddb3cc
Kolja Dummann <k.dummann@gmail.com>
2012-10-15 06:56:27 +0200

    move http verbs enum into a single file

---
+++ Server/HttpVerb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleHttpServer
+{
+    public enum HttpVerb
+    {
+        Get,
+        Post,
+        Put,
+        Delete,
+        Head,
+        Patch
+    }
+}
