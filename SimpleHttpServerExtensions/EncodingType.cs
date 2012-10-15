ccc45ad1f629686e757afe27f7839d864a6462a1
Kolja Dummann <k.dummann@gmail.com>
2012-10-15 06:56:38 +0200

    Add encoding types

---
+++ SimpleHttpServerExtensions/EncodingType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleHttpServer
+{
+    public enum EncodingType
+    {
+        Plain, Deflate, Gzip
+    }
+}
