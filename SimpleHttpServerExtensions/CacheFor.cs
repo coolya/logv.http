ebdb044debfcc06ad496c10288bef4e7ebfb0b22
Kolja Dummann <k.dummann@gmail.com>
2012-12-23 11:10:32 +0100

    Add Caching Constants

---
+++ SimpleHttpServerExtensions/CacheFor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleHttpServer
+{
+    public enum CacheFor
+    {
+        ADay = 86400,
+        AnHour = 3600,
+        TwoHours = 7200,
+        AMinute = 60,
+        HalfAnHour = 1800,
+        AWeek = 604800,
+        TwelveHours = 43200
+    }
+}
