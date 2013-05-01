cb188989a4599bd11b01096b5912fc00a5b6abbc
Kolja Dummann <k.dummann@gmail.com>
2013-05-01 10:11:08 +0200

    add commandline parser

---
+++ logv.host/GenericCommandLineParser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logv.host
+{
+    static class GenericCommandLineParser
+    {
+        public static void Parse(IEnumerable<string> parameters)
+        {
+            var enumerator = parameters.GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                var current = enumerator.Current;
+
+                if (current.StartsWith("-"))
+                {
+                    
+                }
+            }
+        }
+        public static void SetUp(string param, Action setter)
+        {
+        }
+
+        public static void SetUp(string param, bool multiple, Action setter)
+        {
+            
+        }
+
+        public static void SetUp(Action setter)
+        {
+            
+        }
+    }
+}
