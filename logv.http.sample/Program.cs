388ba4a48c387a1e3f37d98d8ea1a883a840190f
Kolja Dummann <kolja.dummann@logv.ws>
2014-04-07 21:35:11 +0200

    update sample

--- logv.http.sample/Program.cs
+++ logv.http.sample/Program.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
      Copyright 2012 Kolja Dummann <k.dummann@gmail.com>
 
    Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,6 +18,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace logv.http.sample
 {
@@ -25,16 +26,17 @@ namespace logv.http.sample
     {
         static void Main(string[] args)
         {
-            var server = new Server("[2001:470:1f15:5e3:11c4:492:eb77:f7ab]", 8080);
+			var server = new Server("localhost", 13337);
 
-            server.Get("http://[2001:470:1f15:5e3:11c4:492:eb77:f7ab]:8080/", (req, res) => res.Write("Hello World!").Close());
+			server.Get("http://localhost:13337/", (req, res) => res.Write("Hello World!").Close());
 
             //to have json output uncomment the next line and comment out the previous one.
             //server.Get("http://localhost:13337" ,(req, res) => res.WriteAsJson(new {Hello = "world"}));
 
             server.Start();
-
-            Console.ReadLine();
+			Console.WriteLine("Server running");
+			while (Console.ReadLine() == null)
+				Thread.Sleep(100);
 
             server.Stop();
         }
